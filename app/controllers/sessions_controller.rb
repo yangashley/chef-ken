9e344a6b08064ba11b9738057c52b403220145a6
Robert Richardson <rrichardsonv+github@gmail.com>
2017-01-25 13:41:11 -0600

    users can login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,30 @@
 class SessionsController < ApplicationController
+  include ApplicationHelper
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(email: user_deets[:email])
+    if @user.authenticate(user_deets[:password])
+      login_user
+      flash[:notice] = "Login Successful!"
+      redirect_to profile_path(@user)
+    else
+      flash[:alert] = "Login failed"
+      @errors = ["That combination of username and password can't be found"]
+      render :action => 'new'
+    end
   end
 
   def destroy
+    flash[:notice] = "Thank's for stopping by!"
+    logout_user
+    redirect_to root_path
+  end
+
+private
+  def user_deets
+      params.require(:user).permit(:email, :password)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def logged_in
+    session[:user_id] != nil
+  end
+
+  def current_user
+   @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
