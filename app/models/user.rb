65b1a2630406b96d6ddd00f68b5dc26611ce4ec4
Robert Richardson <rrichardsonv+github@gmail.com>
2017-01-25 13:21:14 -0600

    finished account creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'sessions/new'
+
+  # get 'sessions/create'
+
+  # get 'sessions/destroy'
+
+  get 'signup', to: "users#new"
+
+  post 'signup', to: "users#create"
+
+  get 'profile', to: "users#show"
+
+  root 'categories#index'
 end
