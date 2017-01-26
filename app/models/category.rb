c3442e044b708bb342947a840a53f30cb6b0f601
Robert Richardson <rrichardsonv+github@gmail.com>
2017-01-26 14:45:55 -0600

    added top 5 and test

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   has_many :recipes
+  has_many :recipe_ratings, through: :recipes, source: :ratings
+  def top_5
+    self.recipes.sort{|a, b| a.aggregate_rating <=> b.aggregate_rating}.reverse[0..4]
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tester_category){Category.create!({name: 'Desert'})}
+  let(:tester){User.create!({name: 'test', email: 'test@test.test', password:'test'})}
+  let(:test_setup){
+    Class.new{
+      def self.setup(test_user, test_cat)
+            # puts '1'
+      # puts test_cat
+      6.times do
+         recipe_info = {
+          title: ['woot','we rockin', 'failer', 'brailer', 'sailer', 'yo'],
+          user_id: test_user.id,
+          time: (1..90).to_a.sample,
+          difficulty: ["easy", "medium", "hard"].sample,
+          directions: Faker::Lorem.sentence
+         }
+        test_cat.recipes.create!(recipe_info)
+      end
+      # puts '2'
+      10.times do
+        rating_info = {
+          recipe_id: test_cat.recipes.sample.id,
+          stars: (1..5).to_a.sample
+        }
+
+        test_user.ratings.create!(rating_info)
+      end
+    end
+    }
+  }
+
+  describe "#top 5" do
+    before(:each){test_setup.setup(tester, tester_category)}
+    it 'returns only 5 recipes' do
+      expect(tester_category.top_5.length).to eq 5
+    end
+
+    it 'orders the results by ratings' do
+      expect(tester_category.top_5[0].aggregate_rating).to be >= tester_category.top_5[1].aggregate_rating
+    end
+
+  end
 end
