ef5a6272a0fc8831d7f1d5ef535218790fe29bcd
Robert Richardson <rrichardsonv+github@gmail.com>
2017-01-25 20:10:52 -0600

    Finally done dropped the ingredients table!

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126020100) do
+ActiveRecord::Schema.define(version: 20170126020700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
