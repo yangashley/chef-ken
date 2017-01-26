47de6c02282d4583ad974ee7f08454afa7e50de4
Robert Richardson <rrichardsonv+github@gmail.com>
2017-01-25 20:37:34 -0600

    rolled back changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   root 'recipes#new'
 
+  get 'recipes/new', to: 'recipes#new'
+
+  post 'recipes/new', to: 'recipes#create'
+  # resources :recipes, only: [:create]
+
+  get 'recipes/show'
+
+  put 'recipes/edit'
+
+  delete 'recipes/destroy'
+
+  get 'recipes/update'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
