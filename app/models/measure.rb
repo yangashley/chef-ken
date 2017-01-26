d55a5a4e2445428463502ad738868666552b733c
Robert Richardson <rrichardsonv+github@gmail.com>
2017-01-25 19:56:31 -0600

    added units column to measures table

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -3,17 +3,17 @@ class Measure < ApplicationRecord
   belongs_to :ingredient
   validates :quantity, :presence => true
 
-  # validates :units, inclusion: {in: [
-                          # 'Tbs', 'Cup','Pint',
-                          # 'tsp', 'Quart', 'Gallon',
-                          # 'pound'
-                          # ]}
-  # def pretty_print_ammount
-  #   if self.quantity.to_f > 1.0 && !["Tbs", "tsp"].include?(self.units)
-  #     u_of_measure = [self.units, 's'].join
-  #   else
-  #     u_of_measure = self.units
-  #   end
-  #   "#{self.quantity} #{u_of_measure} of #{self.ingredient.name}"
-  # end
+  validates :units, inclusion: {in: [
+                          'Tbs', 'Cup','Pint',
+                          'tsp', 'Quart', 'Gallon',
+                          'pound'
+                          ]}
+  def pretty_print_ammount
+    if self.quantity.to_f > 1.0 && !["Tbs", "tsp"].include?(self.units)
+      u_of_measure = [self.units, 's'].join
+    else
+      u_of_measure = self.units
+    end
+    "#{self.quantity} #{u_of_measure} of #{self.ingredient.name}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126015038) do
+ActiveRecord::Schema.define(version: 20170126015425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170126015038) do
     t.float    "quantity"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "units"
   end
 
   create_table "ratings", force: :cascade do |t|
